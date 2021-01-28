c5b779a9ef547942ddf511f5e252068e2dafcf88
cukmekerb <cukmekerb+git@gmail.com>
2021-01-28 12:25:15 -0800

    set up caching (

---
+++ cache.js
@@ -0,0 +1,58 @@
+self.importScripts("https://cdn.jsdelivr.net/gh/mozilla/localForage@master/dist/localforage.js")
+self.addEventListener('install', function (e) {
+    return cache_add_all([
+        "index.html",
+        "view.html",
+        "yours.html",
+        "search.html",
+        "view.css",
+        "burger.png",
+        "favicon.ico",
+        "listenr-logo.png"
+    ]);
+});
+
+function is_image(url) {
+    return url.endsWith(".png") || url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".av1") || url.endsWith(".heic") || url.endsWith(".heif") || url.endsWith(".webp") || url.endsWith(".bmp")
+}
+
+function cache_add_all(array) {
+    caches.open("v1-cache").then(function (cache) {
+        for (i in array) {
+            fetch(array[i]).then(function (response) {
+                if (!response.ok) {
+                    throw new TypeError('Bad response status');
+                }
+                localforage.setItem(array[i], Date.now());
+                cache.put(array[i], response);
+            });
+        }
+    });
+}
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            if (e.request.url.startsWith("https://rss-to-json-convert.herokuapp.com/") || e.request.url.startsWith(new URL(self.location).origin) || is_image(e.request.url) || e.request.url.endsWith(".js")) {
+                console.log(e.request.url + " - sending cache")
+                try {
+                    return response || fetch(e.request)
+                }
+                finally {
+                    localforage.getItem(e.request.url).then(timestamp => {
+                        if (Date.now() - timestamp > 20 * 60 * 1000) {
+                            console.log(e.request.url + " - updating cache")
+                            cache_add_all([e.request.url])
+                        }
+                        else {
+                            console.log(e.request.url + " - not updating cache; timestamp is "+timestamp)
+                        }
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                }
+            }
+            console.log(e.request.url + " - not sending cache")
+            return fetch(e.request);
+        })
+    );
+});
