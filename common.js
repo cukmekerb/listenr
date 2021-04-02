86c8d27c5a86b2240888ff54ebec4d47c27e5a45
cukmekerb <cukmekerb+git@gmail.com>
2021-04-02 10:35:32 -0700

    added docs to common.js

--- common.js
+++ common.js
@@ -1,10 +1,12 @@
+/** converts html to plaintext. requires `div#texttester` 
+ * @param {String} html - the html to convert */
 function htmltotext(html) {
     document.getElementById("texttester").innerHTML = html;
     var b = document.getElementById("texttester").innerText || document.getElementById("texttester").textContent;
     return b;
 }
 
-
+/** Saves the current `user` variable to localForage */
 function savecookies() {
     Cookies.set("usingindexed", "true", exp)
     localforage.setItem("user", JSON.stringify(user)).then(() => {
@@ -12,6 +14,7 @@ function savecookies() {
     });
 }
 
+/** runs on keypress on the `rssurl` and `rssurlm` inputs. reditects to view page */
 function checkrss(data) {
     if (data.keyCode == 13) {
         if (document.getElementById("mobileside").classList.contains("leftside")) {
@@ -22,6 +25,7 @@ function checkrss(data) {
         }
     }
 }
+/** runs on keypress on the `AetBh69SERCH99bHm` and `AetBh69SERCH99bH` inputs. redirects to search page */
 function csearch(data) {
     if (data.keyCode == 13) {
         console.log("aaa")
@@ -29,7 +33,9 @@ function csearch(data) {
     }
 }
 
-
+/** checks if a string is a valid URL
+ * @param {String} str - the url to check
+ */
 function validurl(str) {
     var pattern = new RegExp('^(https?:\\/\\/)?' +
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
@@ -39,9 +45,12 @@ function validurl(str) {
         '(\\#[-a-z\\d_]*)?$', 'i');
     return !!pattern.test(str);
 }
+
+/** hides mobile navigation */
 function hidemnav() {
     document.getElementById("mobileside").classList.add("leftside")
 }
+/** shows mobile navigation */
 function showmnav() {
     document.getElementById("mobileside").classList.remove("leftside")
 }
